d9bbdd9ec60194d43cc3d74faa76471cea4ce420
nicholas-marchese26 <155980631+nicholas-marchese26@users.noreply.github.com>
2025-11-14 10:23:15 -0500

    Update gps_distance.c

--- src/main/common/gps_distance.c
+++ src/main/common/gps_distance.c
@@ -1,1 +1,35 @@
+#include "platform.h"
+#include "io/gps.h"
+#include "common/maths.h"
 
+#ifdef USE_GPS
+
+static uint32_t maxDistance = 0;
+
+void gpsDistanceUpdate(void)
+{
+    if (!STATE(GPS_FIX)) {
+        return;
+    }
+
+    static int32_t homeLat = 0;
+    static int32_t homeLon = 0;
+
+    if (ARMING_FLAG(ARMED) && homeLat == 0) {
+        homeLat = gpsSol.llh.lat;
+        homeLon = gpsSol.llh.lon;
+    }
+
+    uint32_t d = gpsDistance(homeLat, homeLon, gpsSol.llh.lat, gpsSol.llh.lon);
+
+    if (d > maxDistance) {
+        maxDistance = d;
+    }
+}
+
+uint32_t getMaxDistance(void)
+{
+    return maxDistance;
+}
+
+#endif
