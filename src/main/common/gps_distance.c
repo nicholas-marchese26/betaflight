5311f833a6372e2217f0400635ca7402de32f7f7
nicholas-marchese26 <155980631+nicholas-marchese26@users.noreply.github.com>
2025-11-14 11:22:31 -0500

    Update gps_distance.c
    added float getMax3DDistanceMeters(void) and getmaxaltitude

--- src/main/common/gps_distance.c
+++ src/main/common/gps_distance.c
@@ -1,4 +1,6 @@
+#include <math.h>
 #include "platform.h"
+#include "position.h"
 #include "io/gps.h"
 #include "common/maths.h"
 
@@ -27,9 +29,46 @@ void gpsDistanceUpdate(void)
     }
 }
 
+
+float getMaxAltitudeMeters(void)
+{
+    // Update current altitude
+    calculateEstimatedAltitude();
+
+    // Convert to meters
+    float currentAltitudeM = zeroedAltitudeCm / 100.0f;
+
+    // Static variable to hold the max altitude across calls
+    static float maxAltitudeM = 0.0f;
+
+    // Update max if current altitude is higher
+    if (currentAltitudeM > maxAltitudeM) {
+        maxAltitudeM = currentAltitudeM;
+    }
+
+    return maxAltitudeM;
+}
+
+
+
 uint32_t getMaxDistance(void)
 {
     return maxDistance;
+}
+
+
+float getMax3DDistanceMeters(void)
+{
+    // Get horizontal distance in meters
+    uint32_t horizontalM = getMaxDistance();
+
+    // Get max altitude in meters
+    float maxAltM = getMaxAltitudeMeters();
+
+    // 3D distance
+    float max3DDistance = sqrtf(horizontalM * horizontalM + maxAltM * maxAltM);
+
+    return max3DDistance;
 }
 
 #endif
